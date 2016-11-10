6b4b51fb69a190655ef5c94a525abf6e92f82c05
Maclean, James <James.Maclean@fil.com>
2016-11-10 17:16:48 +0000

    first draft ready
    bg images in, styling hacked in

--- app/js/custom.js
+++ app/js/custom.js
@@ -1,19 +1,20 @@
-// window.onload = function onLoad() {
-//     var circle = new ProgressBar.Circle('#progress', {
-//         color: '#FCB03C',
-//         duration: 3000,
-//         easing: 'easeInOut'
-//     });
-//
-//     circle.animate(1);
-// };
+// With the element initially hidden, we can show it slowly:
+function overlayFadeIn() {
+  $( '.js-overlay' ).fadeIn( "slow" );
+  $('html').addClass('no-scroll');
+};
+
+function overlayFadeOut() {
+  $( '.js-overlay' ).fadeOut( "slow" );
+  $('html').removeClass('no-scroll');
+};
 
 
 
-var circle = new ProgressBar.Circle('#progress', {
-    color: '#555',
-    trailColor: '#eee',
-    strokeWidth: 10,
+var circle = new ProgressBar.Circle('.js-loadingModal--circle', {
+    color: '#006193',
+    trailColor: '#d9d9d9',
+    strokeWidth: 6,
     duration: 2500,
     easing: 'linear',
     text: {
@@ -23,7 +24,7 @@ var circle = new ProgressBar.Circle('#progress', {
 
         // Class name for text element.
         // Default: 'progressbar-text'
-        className: 'progressbar__label',
+        className: 'loadingModal--label',
 
         // Inline CSS styles for the text element.
         // If you want to modify all CSS your self, set null to disable
@@ -34,21 +35,7 @@ var circle = new ProgressBar.Circle('#progress', {
         // If you specify anything in this object, none of the default styles
         // apply
         // Default: object speficied below
-        style: {
-            // Text color.
-            // Default: same as stroke color (options.color)
-            color: '#f00',
-            position: 'absolute',
-            left: '50%',
-            top: '50%',
-            padding: 0,
-            margin: 0,
-            // You can specify styles which will be browser prefixed
-            transform: {
-                prefix: true,
-                value: 'translate(-50%, -50%)'
-            }
-        },
+        style: null,
 
         // Only effective if the text.style is not null
         // By default position: relative is applied to container if text
@@ -80,14 +67,14 @@ function to90() {
             duration: 7000,
             step: updatePercentage
         }, function() {
-            to100slow();
+            to99slow();
         });
 
 };
 
-function to100slow() {
+function to99slow() {
 
-    circle.animate(1.0, {
+    circle.animate(0.99, {
         duration: 6000,
         easeing: 'easeOutExpo',
         step: updatePercentage
@@ -95,9 +82,9 @@ function to100slow() {
 
 };
 
-function to100slow() {
+function to99slow() {
 
-    circle.animate(1.0, {
+    circle.animate(0.99, {
         duration: 6000,
         easeing: 'easeOutExpo',
         step: updatePercentage
@@ -108,25 +95,52 @@ function to100slow() {
 function to100fast() {
 
     circle.animate(1.0, {
-        duration: 500,
+        duration: 800,
         easing: 'easeOutExpo',
         step: updatePercentage
+    }, function() {
+      bgAfter();
+      window.scrollTo(0, 0);
+      overlayFadeOut();
     });
 
 };
 
 
-function to99full() {
+function to97full() {
 
-    circle.animate(0.99, {
+    circle.animate(0.97, {
         duration: 10000,
         step: updatePercentage
     });
 
 };
 
+var progressUpdates = [
+  "Checking your login details…",
+  "Loading your account information…",
+  "Fetching  investment valuations…"];
+
+function bgAfter() {
+  $('.wrap').addClass('bg-after-img');
+  $('.wrap').removeClass('bg-before-img');
+};
+
+function bgBefore() {
+  $('.wrap').addClass('bg-before-img');
+  $('.wrap').removeClass('bg-after-img');
+};
 
 
+$( document ).on( 'click', '.js-animate-overlay-in', function( e ) {
+    overlayFadeIn();
+    e.preventDefault();
+});
+
+$( document ).on( 'click', '.js-animate-overlay-out', function( e ) {
+    overlayFadeOut();
+    e.preventDefault();
+});
 
 $( document ).on( 'click', '.js-animate-start', function( e ) {
     to90();
@@ -139,13 +153,13 @@ $( document ).on( 'click', '.js-animate-end', function( e ) {
 });
 
 $( document ).on( 'click', '.js-animate-start-full', function( e ) {
-    to99full();
+    to97full();
     e.preventDefault();
 });
 
 $( document ).on( 'click', '.js-reset', function( e ) {
     circle.set(0.0);
     circle.setText('0%');
-
+    bgBefore();
     e.preventDefault();
 });
